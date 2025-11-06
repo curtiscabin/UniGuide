f58006bf4b68a82fb94a5e304794f6bbb6567bf8
dimasik <ashikhmin.dima@inbox.ru>
2025-11-07 02:08:57 +0500

    added border_radius and right view of text of building

--- UniGuide/main/models.py
+++ UniGuide/main/models.py
@@ -5,7 +5,7 @@ class Building(models.Model):
     """Модель для хранения информации о корпусах университета"""
 
     # Номер корпуса (уникальный)
-    number = models.IntegerField(unique=True, verbose_name="Номер корпуса")
+    number = models.IntegerField(unique=True, verbose_name="Номер корпуса", blank = False)
 
     # CSS позиционирование - это определяет ГДЕ на экране будет корпус
     position_left = models.CharField(
@@ -38,6 +38,29 @@ class Building(models.Model):
         help_text="CSS polygon для сложных форм (оставьте пусто если обычный прямоугольник)"
     )
 
+    #Скругление корпусов
+    border_radius = models.CharField(
+        max_length=50,
+        blank=True,  # Поле не обязательное
+        verbose_name="Скругление углов (border-radius)",
+        help_text="Например: 10px или 10px 0 10px 0"
+    )
+
+    #Отступы для текста
+    text_padding_top = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Отступ текста сверху",
+        help_text="Например: 10px или 2em"
+    )
+
+    text_padding_left = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Отступ текста слева",
+        help_text="Например: 15px"
+    )
+
     class Meta:
         verbose_name = "Корпус"
         verbose_name_plural = "Корпуса"
@@ -45,3 +68,5 @@ class Building(models.Model):
 
     def __str__(self):
         return f"Корпус {self.number}"
+
+
