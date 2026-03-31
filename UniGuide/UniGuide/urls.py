957921977617a74c0832e70abd4bb15e17f4432a
dimasik <ashikhmin.dima@inbox.ru>
2026-04-01 01:47:07 +0500

    search

--- UniGuide/UniGuide/urls.py
+++ UniGuide/UniGuide/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index, building_detail
+from main.views import index, building_detail, search
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index),
     path("building/<int:number>/", building_detail, name="building"),
+    path('search/', search, name='search'),
 ]
